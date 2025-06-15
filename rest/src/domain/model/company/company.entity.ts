bc3d1ca94755cd1deda8f9295116b8c527afcaed
Roger Batista Moreira <rogerbatistamoreira@gmail.com>
2025-06-15 17:27:43 +0200

    fix(global): minnor fixes

--- rest/src/domain/model/company/company.entity.ts
+++ rest/src/domain/model/company/company.entity.ts
@@ -1,6 +1,10 @@
-export class Company {
+import { BaseEntity } from '../base.entity.abstract';
+
+export class Company extends BaseEntity {
   constructor(
     public readonly id: string | null,
     public readonly name: string,
-  ) {}
+  ) {
+    super(id);
+  }
 }
